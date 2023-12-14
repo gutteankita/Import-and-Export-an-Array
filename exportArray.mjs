9c939b667ce4df4683ffb8641a2af39de64e6a9b
gutteankita <ankitagutte2007@gmail.com>
2023-12-14 16:29:20 +0000

    import export

---
+++ exportArray.mjs
@@ -0,0 +1,5 @@
+const array = ['countries', 190, 'continents', 7, false, 6.2]
+export default array
+
+git remote add origin https://github.com/gutteankita/Import-and-Export-an-Array.git
+
---
+++ importArray.mjs
@@ -0,0 +1,4 @@
+
+
+import array from './exportArray.mjs';  // Use a relative path
+console.log(array);
